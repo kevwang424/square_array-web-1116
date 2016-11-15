9635e3475b9026824d0724af91ca0b2db5aba58a
Kevin Wang <kev.wang424@gmail.com>
2016-11-15 18:29:45 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
+#using each method. Returns original array so need to declare a new array variable
 def square_array(array)
-  # your code here
+  answer = []
+  array.each {|num| answer << num ** 2}
+  answer
 end
+
+#map/collect returns an array after doing whatever in the code block
+# def square_array(array)
+#   array.collect {|num| num ** 2}
+# end
